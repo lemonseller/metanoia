b625f1d61307d413c209c005962eabd5337fdf04
Yuri <dragonrobot29@gmail.com>
2024-03-03 09:26:26 -0700

    Implementation of Google Oauth

---
+++ public/home/home.js
@@ -0,0 +1,31 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+  
+  
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+
+
+}
+gapi.load('auth2', function() {
+    gapi.auth2.init();
+  });
+
+  document.getElementById('googleSignIn').onclick = function() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signIn().then(function(googleUser) {
+      var profile = googleUser.getBasicProfile();
+      console.log('ID: ' + profile.getId());
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail());
+    });
+  };
