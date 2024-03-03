b055a30ef9b4346d8031ab69fc625a0165610e08
Warren Fu <whfu2004@gmail.com>
2024-03-03 11:20:16 -0700

    added redirect after login

--- public/home/home.js
+++ public/home/home.js
@@ -1,11 +1,18 @@
+import { gapi } from 'gapi'; // Add the missing import statement for the gapi object
+
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
-  
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+  // Store the email in localStorage
+  localStorage.setItem('email', profile.getEmail());
+
+  // Redirect to loggedin.html
+  window.location.href = 'loggedin.html';
+}
   
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
